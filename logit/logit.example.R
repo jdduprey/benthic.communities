797aa373dd0a1808aa1e0027c9231cb7e23c4eb4
jdduprey <joe.d.duprey@gmail.com>
2021-05-19 09:18:59 -0700

    benthic presence/absence csv

--- logit/logit.example.R
+++ logit/logit.example.R
@@ -64,5 +64,5 @@ head(newdata3)
 
 ggplot(newdata3, aes(x = gre, y = PredictedProb)) + geom_ribbon(aes(ymin = LL,
   ymax = UL, fill = rank), alpha = 0.2) + geom_line(aes(colour = rank),
-                                                                                                                      size = 1)
+  size = 1)
 
