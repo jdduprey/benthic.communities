6eb95ec873042172a504a0c98a48c8603ec82bf0
jdduprey <joe.d.duprey@gmail.com>
2021-06-08 15:02:36 -0700

    Create benthic.boxplots.R

---
+++ code /benthic.boxplots.R
@@ -0,0 +1,11 @@
+#===================================================
+# Joe Duprey
+# Benthic diversity boxplots
+# overall species richness, algae vs inverts, 
+# and by phylum/higher taxonomy
+# last edited 06/08/2021
+#===================================================
+
+library('tidyverse')
+library('dplyr')
+library('ggplot2')
