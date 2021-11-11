022314600db74b8ea3183b0d1ca85fd556d26e2a
jdduprey <joe.d.duprey@gmail.com>
2021-11-10 16:37:25 -0800

    making some good progress on NMDS plots, but sometimes they don't converge...
    also unsure why they look good this time, contrast with last attempt

---
+++ code /jacard_PCA.R
@@ -0,0 +1,119 @@
+#===================================================
+# Joe Duprey
+# Jaccard PCA plot - algal communities? 
+# Code to view PCA plots for benthic, macroalgal, microalgal communities
+# Last edited 11/10/2021
+#===================================================
+library("viridis")
+library("tidyverse")
+library("dplyr")
+library("ggplot2")
+library("vegan")
+
+by.sample.species <- read.csv('../data/by.sample.species.csv') # reads merged by tech and bio
+benthic.presence.absence <- read.csv('../data/benthic.presence.absence.csv')
+hash.annotated <- read.csv('../data/hash.annotated.csv') # Mocho's hashes with taxa info
+events <- read.csv('../data/events.joe.format.csv') # event table
+more_hashes <- read.csv("../data/all.taxonomy.20190130.csv")
+
+# split date and site in event table
+# sd.events <- events %>% 
+#   separate(col=sample, remove=FALSE, into=c("site", "date"), sep = '_')
+
+# merge hashes by species - is this correct 
+species.annotated <- hash.annotated %>%
+  distinct(species, .keep_all=TRUE) 
+
+# joe's first attempt to filter by benthic algea (micro and macro) and 
+# invertebrates (micro and macro)
+
+# benthic_algae <- species.by.sample.alltax %>%
+#   filter(phylum %in% c("Florideophyceae", "Phaeophyceae", "Bacillariophyta",
+#                        "Bangiophyceae", "Compsopogonophyceae", "Rhodophyta"))
+# 
+# 
+# benthic_inverts <- species.by.sample.alltax %>%
+#   filter(phylum %in% c("Cnidaria", "Arthropoda", "Annelida", "Mollusca",
+#                        "Bryozoa", "Echinodermata", "Nemertea", "Entoprocta",
+#                        "Brachiopoda", "Nematoda"))
+
+arthropod_list <- c("Arthropoda")
+ben_algae_list <- c("Florideophyceae", "Phaeophyceae", "Bacillariophyta", 
+                    "Bangiophyceae", "Compsopogonophyceae", "Rhodophyta")
+ben_invert_list <- c("Cnidaria", "Arthropoda", "Annelida", "Mollusca",
+                     "Bryozoa", "Echinodermata", "Nemertea", "Entoprocta",
+                     "Brachiopoda", "Nematoda")
+
+# below is a flexible function get get different forms of PA data 
+#===================================================
+
+
+species.by.sample.alltax <- left_join(by.sample.species, species.annotated, by='species')
+
+# FUNCTION takes as input df with all present sampling events and annotations 
+# for example species.by.sample.alltax in this script 
+# returns long form presence and absence data with the selected phyla included
+filter_get_PA_data <- function(all_tax_df, phyla_list) {
+
+  filtered_df <- all_tax_df %>%
+    filter(phylum %in% phyla_list) %>%
+    filter(benthos %in% c("BEN", "Both")) 
+  
+  n_detections_taxa_df <- filtered_df %>%
+    group_by(sample, species, .drop=FALSE) %>%
+    summarise(richness = n()) %>%
+    ungroup() %>%
+    complete(sample, species,
+             fill = list(richness = 0)) %>%
+    separate(sample, into = c("site","date" ), sep = "_", remove = F) %>%
+    separate(date, into = c("year", "month"), sep = 4, remove = F)
+  
+  # now on to the jacard PCA part of the function
+  meta_df <- n_detections_taxa_df %>%
+    select(sample, site, date, year, month) %>%
+    distinct(sample, site, date, year, month)
+  
+  # get data into the right form for vegan PCA 
+  wide_pa <- n_detections_taxa_df %>%
+    select(sample, species, richness) %>%
+    pivot_wider(names_from = species, values_from = richness) %>%
+    select(-sample)
+  
+  # get correct format for jaccard 
+  
+  # glorious list of things I want to return 
+  taxa_data_list <- list("PA_df" = n_detections_taxa_df, 
+                         "metadata" = meta_df,
+                         "wide_PA" = wide_pa)
+  
+  return(taxa_data_list)
+  }
+
+# test filter_get_PA_data function 
+algae_data <- filter_get_PA_data(species.by.sample.alltax, ben_algae_list)
+
+jaccard_nmds <- metaMDS(algae_data$wide_PA, distance = "jaccard")
+jaccard_MDS1 <- jaccard_nmds$points[,1] #store nmds values
+jaccard_MDS2 <- jaccard_nmds$points[,2] #store nmds values 
+
+# meta_test <- algae_data$metadata
+
+jaccard_to_plot <- cbind(algae_data$metadata, jaccard_MDS1, jaccard_MDS2)
+
+
+ggplot(raw.pca.to.plot, aes(x=jaccard_MDS1, y=jaccard_MDS2)) +
+  geom_point(size=3, aes(color=factor(site))) +  # shape=factor())
+  theme_bw() +
+  labs(x="PC1",y="PC2", color="Site") +
+  ggtitle('COI - Jaccard PA')  # + geom_text(aes(label=sample))
+
+ggsave("../figures/nonconverg_inv_eily_PCA.png")
+dev.off()
+
+# this code find the number of times each species occurs in our data 
+# useful to have for analysis and knowing which questions we can answer
+sum_detections_taxa <- n_detections_taxa %>%
+  group_by(species) %>%
+  mutate(sum_detections = sum(richness)) %>%
+  distinct(species, sum_detections)
+
