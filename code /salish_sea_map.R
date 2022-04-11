e50a3619a16f6563de23d71009db9d669500c9a5
jdduprey <joe.d.duprey@gmail.com>
2022-04-11 08:52:12 -0700

    tidying up

--- code /salish_sea_map.R
+++ code /salish_sea_map.R
@@ -1,3 +1,11 @@
 
-
-library(ggplot2)
+# Load package
+library(marmap)
+#  Fetch data on NOAA servers and write on disk
+bat <- getNOAA.bathy(-126, -122, 47, 49, res = 1, keep=TRUE)
+# Create nice looking color palettes
+blues <- c("lightsteelblue4", "lightsteelblue3", "lightsteelblue2", "lightsteelblue1")
+greys <- c(grey(0.6), grey(0.93), grey(0.99))
+# Plot
+plot(bat, image = TRUE, land = TRUE, lwd = 0.1, bpal = list(c(0, max(bat), greys), c(min(bat), 0, blues)))
+plot(bat, lwd = 0.8, deep = 0, shallow = 0, step = 0, add = TRUE) # highlight coastline
